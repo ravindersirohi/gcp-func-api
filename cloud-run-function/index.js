a393d7c35834de6c96f81e55cc46df65923229f0
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 14:02:01 +0000

    auth updated

--- cloud-run-function/index.js
+++ cloud-run-function/index.js
@@ -1,21 +1,19 @@
 
 import { http } from '@google-cloud/functions-framework';
-// const { GoogleAuth } = require('google-auth-library');
+import { GoogleAuth } from 'google-auth-library';
 
 http('helloGithub', async (req, res) => {
-  
-  // Calling securec cloud function.
-  const url = 'https://secure-func-817570901624.europe-west2.run.app';
-  const targetAudiance = url;
-  // const auth = new GoogleAuth();
-
-  try{
+  try {
+    // Calling securec cloud function.
+    const url = 'https://secure-func-817570901624.europe-west2.run.app';
+    const targetAudiance = url;
+    const auth = new GoogleAuth();
     // const client = await auth.getIdTokenClient(targetAudiance);
     // const secureFuncResponse = await client.request({url});
     // res.send('{message: secureFuncResponse.data}')
     res.send('Hello from Github func!')
   }
-  catch(err){
+  catch(err) {
     res.send({message: err.message})
   }
 });
