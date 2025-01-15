f87f4ec34ff9d4174d5ad2e38b6da92081b39d21
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 11:41:46 +0000

    Extended to call secure

--- cloud-run-function/index.js
+++ cloud-run-function/index.js
@@ -1,6 +1,19 @@
 
 import { http } from '@google-cloud/functions-framework';
 
-http('helloGithub', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'from Github!'}!`);
+http('helloGithub', async (req, res) => {
+  
+  // Calling securec cloud function.
+  const url = 'https://secure-func-817570901624.europe-west2.run.app';
+  const targetAudiance = url;
+  const auth = new GoogleAuth();
+
+  try{
+    const client = await auth.getIdTokenClient(targetAudiance);
+    const funcDemoResp = await client.request({url});
+    res.send({message: funcDemoResp.data})
+  }
+  catch(err){
+    res.send({message: err.message})
+  }
 });
