8ed18622e6cff054411d30d4b9ed17381f7dcbb1
Ravinder Sirohi <ravinder@hovergen.com>
2025-01-15 12:27:03 +0000

    Just taken off sec fun call.

--- cloud-run-function/index.js
+++ cloud-run-function/index.js
@@ -1,18 +1,19 @@
 
 import { http } from '@google-cloud/functions-framework';
-const { GoogleAuth } = require('google-auth-library');
+// const { GoogleAuth } = require('google-auth-library');
 
 http('helloGithub', async (req, res) => {
   
   // Calling securec cloud function.
   const url = 'https://secure-func-817570901624.europe-west2.run.app';
   const targetAudiance = url;
-  const auth = new GoogleAuth();
+  // const auth = new GoogleAuth();
 
   try{
-    const client = await auth.getIdTokenClient(targetAudiance);
-    const secureFuncResponse = await client.request({url});
-    res.send({message: secureFuncResponse.data})
+    // const client = await auth.getIdTokenClient(targetAudiance);
+    // const secureFuncResponse = await client.request({url});
+    // res.send('{message: secureFuncResponse.data}')
+    res.send('Hello from Github func!')
   }
   catch(err){
     res.send({message: err.message})
